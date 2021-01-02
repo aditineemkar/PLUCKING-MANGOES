52018b7ee3aa1ba41dc944846ebbd4237a32bbec
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-02 14:26:15 +0530

    Update boy.js

--- boy.js
+++ boy.js
@@ -1,24 +1,18 @@
-class Boy{
-    constructor(x,y){
-        var options = {
-            'friction':1,
-            'density':1.5,
-            isStatic : true
-        }
-    this.body= Bodies.rectangle(x,y,50,50,options);
-    this.width=300;
-    this.height=400;
-    this.image=loadImage("cow.png");
-    World.add(world,this.body)
+class Boy {
+    constructor(x, y, width, height) {
+  
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage('cow.png');
+      World.add(world, this.body);
+
     }
+
     display(){
         var pos = this.body.position;
-        push();
-        translate(pos.x,pos.y);
-        strokeWeight(4);
-        stroke("blue");
-        fill("red");
         imageMode(CENTER);
-        image(this.image,0,0,this.width,this.height);
+        image(this.image, pos.x, pos.y, this.width, this.height);
     }
-} 
+
+  };
