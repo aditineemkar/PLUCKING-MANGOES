6b76c638aacc109e473c1e3d3425baa3dff83ed0
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-02 14:27:09 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,19 +1,17 @@
-class Ground
-{
-    constructor(x,y,width,height){
-        var ground_options={
-            isStatic:true
-        }
-        this.body = Bodies.rectangle(x,y,width,height,ground_options);
-        this.width=width;
-        this.height=height;
-
-        World.add(world,this.body);
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
     }
     display(){
-        var post = this.body.position;
-        rectMode(CENTER);
-        fill("brown");
-        rect(post.x,post.y,this.width,this.height);
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
     }
-}
+  };
