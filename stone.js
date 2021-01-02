6d5e6bc121bf38f287ad46d28a91b7733e841411
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-02 14:30:15 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -1,26 +1,19 @@
-class Stone{
-    constructor(x,y,r){
-        var options = {
-            isStatic : false,
-            friction:1,
-			density:1.2,
-			restitution : 0
-        }
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.image = loadImage("stone.png");
-        this.body = Bodies.circle(this.x,this.y,this.r,options);
-        World.add(world,this.body);
+class Stone {
+    constructor(x, y, width, height) {
+      
+      var options = {isStatic: true};
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage('stone.png');
+      World.add(world, this.body);
+
     }
-    display() {
+
+    display(){
         var pos = this.body.position;
-        push();
-        translate(pos.x,pos.y);
-        fill(255,0,255)
-        imageMode(CENTER)
-        ellipseMode(RADIUS)
-        image(this.image, 0, 0, this.r*2, this.r*2)
-        pop();
-      }
-}
+        imageMode(CENTER);
+        image(this.image, pos.x, pos.y, this.width, this.height);
+    }
+
+  };
